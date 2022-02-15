8f3790e5451df03e132910a87363edfb9014a52f
Mikaell-Amidi <mikael@Erfans-MacBook-Pro.local>
2022-02-15 15:50:43 +0330

    syncing changes

--- module.001/src/main/java/com/nordic/rover/modular/StateMachineContext.java
+++ module.001/src/main/java/com/nordic/rover/modular/StateMachineContext.java
@@ -1,16 +1,13 @@
 package com.nordic.rover.modular;
 
-import com.nordic.rover.modular.base.adopter.StateMachineObserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class StateMachineContext {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(StateMachineContext.class, args);
-        context.getBean(StateMachineObserver.class).changeState();
+        SpringApplication.run(StateMachineContext.class, args);
     }
 
 }
